9e1e6752fb2635f58f15bcb52d55d7d44de3ea66
Korsar21 <kosart2006@gmail.com>
2025-12-24 19:33:36 +0300

    Only lab_3_4

--- lab_game_tg_bot/game.py
+++
@@ -1,8 +0,0 @@
-from room import Room
-from player import Player
-
-class Game:
-    def __init__(self, room: Room, player: Player, counter_room: int):
-        self.current_room = room
-        self.user = player
-        self.counter_room = counter_room
