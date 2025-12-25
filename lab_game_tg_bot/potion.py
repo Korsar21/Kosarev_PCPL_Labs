9c8c3370000d7da3ba457bc07e404fe42ef285d2
Korsar21 <kosart2006@gmail.com>
2025-12-25 12:54:24 +0300

    Only lab_habit_tg_bot

--- lab_game_tg_bot/potion.py
+++
@@ -1,6 +0,0 @@
-import random
-
-class Potion:
-    def __init__(self):
-        self.amount_restored_health = random.randint(5, 12)
-
--- lab_python_oop/figure.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class GeometricFigure(ABC):
-    @abstractmethod
-    def area(self):
-        pass
